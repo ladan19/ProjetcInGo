4ba4c4bf673bd0c5cb1115885960328eb9a71913
ladan19 <ladandecsm@gmail.com>
2021-12-22 04:02:32 +0000

    first commit

--- booking-app/main.go
+++ booking-app/main.go
@@ -3,7 +3,15 @@ package main
 import "fmt"
 
 func main() {
+	var conferenceName = "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
 
-	fmt.Println("Hello World")
+	fmt.Println("Welcome to", conferenceName, "booking applicarion")
+	fmt.Println("We have total of", conferenceTickets, "tickets and", remainingTickets, "are still available.")
+	fmt.Println("Get your tickets here to attend")
+
+	
+	
 }
 
