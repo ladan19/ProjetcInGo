abebf2b948f27917a4f898305b07821e5bfd9f54
ladan19 <ladandecsm@gmail.com>
2022-01-12 16:21:30 +0000

    add new feature

--- booking-app/main.go
+++ booking-app/main.go
@@ -7,17 +7,34 @@ func main() {
 	const conferenceTickets = 50
 	var remainingTickets = 50
 
+	fmt.Printf("conferenceName is %T, conferenceTickets is %T\n", conferenceName, conferenceTickets)
+
 	fmt.Printf("Welcome to %v booking applicarion\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var userName string
+	var firtsName string
+	var lastName string
+	var email string
 	var userTicket int
 	// ask user for their name and ticket
 
-	userName = "Ladan"
-	userTicket = 2
+	// memory location of this variable
+	fmt.Println(&conferenceName)
+
+	// & before variable is a pointer to its memory address
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firtsName)
+
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email name: ")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter number of tickets: ")
+	fmt.Scan(&userTicket)
 
 	// I'm using printf to do this
-	fmt.Printf("User %v booked %v tickets\n", userName, userTicket)
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmantion email at %v \n", firtsName, lastName, userTicket, email)
 }
