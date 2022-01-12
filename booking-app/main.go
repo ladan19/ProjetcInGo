c1983d66bd1914b29dd8cd8b38825c38bdc869c4
ladan19 <ladandecsm@gmail.com>
2022-01-12 15:59:06 +0000

    New commit

--- booking-app/main.go
+++ booking-app/main.go
@@ -7,11 +7,17 @@ func main() {
 	const conferenceTickets = 50
 	var remainingTickets = 50
 
-	fmt.Println("Welcome to", conferenceName, "booking applicarion")
-	fmt.Println("We have total of", conferenceTickets, "tickets and", remainingTickets, "are still available.")
+	fmt.Printf("Welcome to %v booking applicarion\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	
-	
-}
+	var userName string
+	var userTicket int
+	// ask user for their name and ticket
 
+	userName = "Ladan"
+	userTicket = 2
+
+	// I'm using printf to do this
+	fmt.Printf("User %v booked %v tickets\n", userName, userTicket)
+}
